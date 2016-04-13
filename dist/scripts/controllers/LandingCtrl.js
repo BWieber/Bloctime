25b825c8e5591ee8a17cc7360fcbe14a01014ab6
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-04-13 17:10:29 -0400

    startTimer() and stopTimer() functions added

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -4,10 +4,21 @@
          
          this.clock = 1500;
          
-         var interval = $interval(function () {
-             this.clock -= 1;
-         }.bind(this), 1000);
+         this.timer = null;
          
+         this.startTimer = function() {
+             this.timer = $interval(function () {
+                 this.clock -= 1;
+             }.bind(this), 1000);
+         }
+         
+         this.stopTimer = function() {
+             if (angular.isDefined(this.timer)) {
+                    $interval.cancel(this.timer);
+             }
+         }
+         
+        
      }
  
      angular
