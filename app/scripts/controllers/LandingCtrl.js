a2800a60e8a7f78699ae4d0941ba40b1dc2ed72a
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-04-14 20:17:41 -0400

    Button Text logic added

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -6,15 +6,24 @@
          
          this.timer = null;
          
+         this.buttonMsg = "Time to Pomodoro!";
+         
+         this.timerRunning = false;
+         
          this.startTimer = function() {
+             this.timerRunning = true;
+             this.buttonMsg = "Reset Timer"
              this.timer = $interval(function () {
                  this.clock -= 1;
              }.bind(this), 1000);
          }
          
-         this.stopTimer = function() {
+         this.resetTimer = function() {
              if (angular.isDefined(this.timer)) {
                     $interval.cancel(this.timer);
+                    this.clock = 1500;
+                    this.timerRunning = false;
+                    this.buttonMsg = 'Ready to Pomodoro again?'
              }
          }
          
